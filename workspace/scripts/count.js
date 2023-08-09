ab031075ba107c8d0bb8ab15af116971467faac7
AmauryD <deflorenne.amaury@gmail.com>
2023-08-09 15:55:57 +0200

    count

--- workspace/scripts/count.js
+++ workspace/scripts/count.js
@@ -2,5 +2,7 @@ let db = connect("mongodb://root:test123*@localhost:27017");
 // use technocite;
 db = db.getSiblingDB("sample_mflix");
 
-// récupérer max 5 films
-const movies = db.movies.find().limit(5);
+// compter le nombre de films dans le résultat
+const movies = db.movies.find().count();
+
+console.log(movies);
