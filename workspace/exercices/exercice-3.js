23f4527100893cf3a871d9128e0ec2ec89a991ef
AmauryD <deflorenne.amaury@gmail.com>
2023-08-11 15:04:50 +0200

    aggregations exercice

---
+++ workspace/exercices/exercice-3.js
@@ -0,0 +1,29 @@
+const db = connect("mongodb://root:test123*@localhost:27017").getSiblingDB('sample_mflix');
+
+const movies = db.movies.aggregate([
+    {
+        $match: {
+            'imdb.rating': {
+                $lt: 5
+            }
+        }
+    },
+    {
+        $unwind: '$directors'
+    },
+    {
+        $group: {
+            _id: '$directors',
+            total : {
+                $count: {}
+            }
+        }
+    },
+    {
+        $sort: {
+            total: -1
+        }
+    }
+]);
+
+console.log(movies);
