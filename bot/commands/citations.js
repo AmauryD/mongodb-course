ae3448a5a37ea600b6ead4f15ba7ed29624024c3
AmauryD <deflorenne.amaury@gmail.com>
2023-08-16 14:24:22 +0200

    split database connection

--- bot/commands/citations.js
+++ bot/commands/citations.js
@@ -1,19 +1,17 @@
+export async function citationCommand(interaction, databaseConnection) {
+    const collection = databaseConnection.collection("citations");
 
-const citations = [
-    "Dans l'obscurité de l'incertitude, la lumière de la connaissance guide nos pas.",
-    "Les rêves d'aujourd'hui façonnent les réalités de demain, car la pensée forge la réalité.",
-    "Le temps, ce flux inéluctable, sculpte nos vies comme un artiste invisible.",
-    "Les frontières de la réalité sont définies par les limites de notre perception.",
-    "L'âme humaine est un océan profond où les émotions dansent telle une marée éternelle.",
-    "La sagesse est le trésor que l'on accumule lorsque l'on creuse dans les mines de l'expérience.",
-    "Les questions sans réponses sont les étoiles qui guident la barque de notre quête de sens.",
-    "La liberté réside dans la compréhension de soi, car seul celui qui se connaît peut être véritablement libre.",
-    "Comme les feuilles qui tombent en automne, les souvenirs sont les vestiges de notre passage dans le jardin du temps.",
-    "La beauté réside dans les yeux de celui qui contemple, car chaque regard est une interprétation unique."
-];
+    // récupère toutes les citations de la collection citations
+    const citations = await collection.find().toArray();
 
-export function citationCommand(interaction) {
+    if (citations.length === 0) {
+        interaction.reply("Désolé, il n'y a aucun citation présente :(");
+        // on s'arrête là
+        return;
+    }
+    
     const randomCitation = citations[Math.floor(Math.random() * citations.length)];
     console.log(interaction.user.displayName, "a fait", interaction.commandName);
-    interaction.reply(randomCitation);
+    interaction.reply(randomCitation.text);
 }
+
--- bot/index.js
+++ bot/index.js
@@ -2,6 +2,7 @@ import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 import { registerCommands } from "./register-commands.js";
 import { citationCommand } from "./commands/citations.js";
+import { connectToDatabase } from "./database-connection.js";
 
 // lis le fichier .env et l'injecte dans le process.env
 dotenv.config({
@@ -12,9 +13,11 @@ dotenv.config({
 const TOKEN = process.env.TOKEN;
 // LE NUMERO UNIQUE DE l'APPLICATION
 const CLIENT_ID = process.env.CLIENT_ID;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 async function init() {
     await registerCommands(TOKEN, CLIENT_ID);
+    const db = await connectToDatabase(DATABASE_URL);
 
     // on crée le client discord
     const client = new Client({
@@ -24,7 +27,7 @@ async function init() {
     // on écoute sur l'évènement "interactionCreate". Qui capture les commandes faites à votre bot
     client.on("interactionCreate", (interaction) => {
         if (interaction.commandName === "citation") {
-            citationCommand(interaction);
+            citationCommand(interaction, db);
         }
     })
 
